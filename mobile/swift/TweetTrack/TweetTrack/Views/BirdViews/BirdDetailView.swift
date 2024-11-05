2546a5828773403befd252533e91bba05d510c65
Mózes Kristóf <mozeskristof2022@gmail.com>
2024-11-05 17:26:12 +0200

    Add map to birdDetailView

--- mobile/swift/TweetTrack/TweetTrack/Views/BirdViews/BirdDetailView.swift
+++ mobile/swift/TweetTrack/TweetTrack/Views/BirdViews/BirdDetailView.swift
@@ -1,7 +1,14 @@
+import MapKit
 import SwiftUI
 
 struct BirdDetailView: View {
     var bird: Bird
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
 
     var body: some View {
         VStack {
@@ -12,8 +19,12 @@ struct BirdDetailView: View {
             Text(bird.description)
                 .font(.body)
                 .padding()
-
             Spacer()
+
+            Map(position: $position, interactionModes: [.all])
+                .frame(height: 300)
+                .cornerRadius(25)
+                .padding()
         }
         .padding()
     }
