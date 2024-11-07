e9209721e8e12a816c7784e16d38d686037478d8
Mózes Kristóf <mozeskristof2022@gmail.com>
2024-11-07 20:59:05 +0200

    Make BirdFetcher async

--- mobile/swift/TweetTrack/TweetTrack/ViewModels/BirdsFetcher.swift
+++ mobile/swift/TweetTrack/TweetTrack/ViewModels/BirdsFetcher.swift
@@ -3,21 +3,20 @@ import Foundation
 class BirdFetcher: ObservableObject {
     @Published var birds: [Bird] = []
 
-    func fetchBirds() {
+    func fetchBirds() async {
         let urlString = Api.birdsEndpoint
         guard let url = URL(string: urlString) else { return }
 
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data, error == nil else { return }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
 
-            do {
-                let decodedData = try JSONDecoder().decode([Bird].self, from: data)
-                DispatchQueue.main.async {
-                    self.birds = decodedData
-                }
-            } catch {
-                print("Error : \(error)")
+            let decodedData = try JSONDecoder().decode([Bird].self, from: data)
+
+            DispatchQueue.main.async {
+                self.birds = decodedData
             }
-        }.resume()
+        } catch {
+            print("Error fetching birds: \(error)")
+        }
     }
 }
