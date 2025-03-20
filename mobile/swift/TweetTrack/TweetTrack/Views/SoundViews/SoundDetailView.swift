c6a714faf2e95b09664889a0fb858337d803615f
Mózes Kristóf <raffles.ground_06@icloud.com>
2025-03-20 19:44:18 +0200

    Update SoundDetailView.swift looks

--- mobile/swift/TweetTrack/TweetTrack/Views/SoundViews/SoundDetailView.swift
+++ mobile/swift/TweetTrack/TweetTrack/Views/SoundViews/SoundDetailView.swift
@@ -3,8 +3,39 @@ import SwiftUI
 struct SoundDetailView: View {
     let sound: BirdSoundItem
     let detectionService: BirdDetectionService
+    private let detectionResult: String
+
+    init(sound: BirdSoundItem, detectionService: BirdDetectionService) {
+        self.sound = sound
+        self.detectionService = detectionService
+        self.detectionResult = detectionService.detectBirdSound(audioURL: URL(fileURLWithPath: sound.audioDataPath))
+    }
+
     var body: some View {
-        Text(sound.title)
-        Text(detectionService.detectBirdSound(audioURL: URL(fileURLWithPath: sound.audioDataPath)))
+        VStack(spacing: 20) {
+            Text(sound.title)
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+
+            Text(detectionResult)
+                .font(.headline)
+                .foregroundColor(.gray)
+                .padding()
+
+            Button(action: {
+                // Call post requesr for identification
+            }) {
+                Text("Identify the Bird")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal, 20)
+        }
+        .padding()
     }
 }
