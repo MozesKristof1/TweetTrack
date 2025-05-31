68992c4c2d02abfb2b65828617bb2d153ba46b1d
Mózes Kristóf <raffles.ground_06@icloud.com>
2025-05-31 15:52:01 +0300

    add search by bird name in bird list view

--- mobile/swift/TweetTrack/TweetTrack/Views/BirdViews/BidListView.swift
+++ mobile/swift/TweetTrack/TweetTrack/Views/BirdViews/BidListView.swift
@@ -2,26 +2,44 @@ import SwiftUI
 
 struct BirdListView: View {
     @StateObject var birdFetcher = BirdFetcher()
+    @State private var searchText = ""
+
+    var filteredBirds: [Bird] {
+        if searchText.isEmpty {
+            return birdFetcher.birds
+        } else {
+            return birdFetcher.birds.filter { bird in
+                bird.name.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
 
     var body: some View {
-        ScrollView {
-            VStack(spacing: 8) {
-                ForEach(birdFetcher.birds) { bird in
-                    NavigationLink(destination: BirdDetailView(bird: bird)) {
-                        BirdCardView(bird: bird)
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 8) {
+                    ForEach(filteredBirds) { bird in
+                        NavigationLink(destination: BirdDetailView(bird: bird)) {
+                            BirdCardView(bird: bird)
+                        }
+                        .buttonStyle(.plain)
                     }
                 }
+                .padding()
             }
-            .padding()
-        }
-        .onAppear {
-            Task{
-                await birdFetcher.fetchBirds()
+            .navigationTitle("Birds")
+            .searchable(text: $searchText, prompt: "Search by bird name")
+            .onAppear {
+                if birdFetcher.birds.isEmpty {
+                    Task {
+                        await birdFetcher.fetchBirds()
+                    }
+                }
             }
         }
     }
 }
 
 #Preview {
-    BirdListView(birdFetcher: BirdFetcher())
+    BirdListView()
 }
