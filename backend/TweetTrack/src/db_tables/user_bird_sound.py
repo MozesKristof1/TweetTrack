bb3c8023d9ffc796460630e6e6375f0a58f819aa
Mózes Kristóf <raffles.ground_06@icloud.com>
2025-06-02 13:27:55 +0300

    update UserBirdSound

--- backend/TweetTrack/src/db_tables/user_bird_sound.py
+++ backend/TweetTrack/src/db_tables/user_bird_sound.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Column, String, ForeignKey, Boolean
+from sqlalchemy import Column, String, ForeignKey, Boolean, LargeBinary
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from . import Base
@@ -9,7 +9,10 @@ class UserBirdSound(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_bird_id = Column(UUID(as_uuid=True), ForeignKey("user_bird.id"), nullable=False)
-    sound_url = Column(String(255), nullable=False)
-    identified = Column(Boolean, nullable=False)
+    sound_data = Column(LargeBinary, nullable=False)
+    file_name = Column(String(255), nullable=True)
+    file_type = Column(String(50), nullable=True)
+    file_size = Column(String(20), nullable=True)
+    identified = Column(Boolean, nullable=False, default=False)
 
     user_bird = relationship("UserBird", back_populates="sounds")
