eacff38a3b571d9afb4faf896d8abbd8f3de37a1
Mózes Kristóf <raffles.ground_06@icloud.com>
2025-06-02 21:11:38 +0300

    remove

--- backend/TweetTrack/api/routers/birds.py
+++ backend/TweetTrack/api/routers/birds.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import Session
 
 from db import get_db
 from models.birdDTO import BirdDTO
-from backend.TweetTrack.api.services.bird_service import get_birds as get_birds_service
+from db_tables import Bird
 
 router = APIRouter()
 
 
 @router.get("/birds", response_model=List[BirdDTO])
 def get_birds(limit: int = 100, db: Session = Depends(get_db)):
-    birds = get_birds_service(db, limit)
+    birds = db.query(Bird).limit(limit).all()
     return birds
 
--- backend/TweetTrack/api/services/bird_service.py
+++ backend/TweetTrack/api/services/bird_service.py
@@ -1,7 +1,8 @@
-from sqlalchemy.orm import Session
-from typing import List
-from backend.TweetTrack.api.repositories.bird_repository import get_birds_db
-from db_tables import Bird
-
-def get_birds(db: Session, limit: int = 100) -> List[Bird]:
-    return get_birds_db(db, limit)
+# from sqlalchemy.orm import Session
+# from typing import List
+#
+# from .bird_repository import get_birds_db
+# from db_tables import Bird
+#
+# def get_birds(db: Session, limit: int = 100) -> List[Bird]:
+#     return get_birds_db(db, limit)
